80faf57103c10a39bfbfc792b9114d85159ab713
betrakiss <angelin.nedelchev@outlook.com>
2016-01-26 15:50:55 +0200

    Adding refactored task one.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,17 +1,14 @@
-def convert_to_bgn(amount, currency)
-  bgn_amount = case currency
-                 when :usd then amount * 1.7408
-                 when :eur then amount * 1.9557
-                 when :gbp then amount * 2.6415
-                 else amount
-               end
+RATES =  {
+  :usd => 1.7408,
+  :eur => 1.9557,
+  :gbp => 2.6415,
+  :bgn => 1.00
+}
 
-  bgn_amount.round(2)
+def convert_to_bgn(amount, currency)
+  (RATES[currency] * amount).round(2)
 end
 
-def compare_prices(first_price, first_currency, second_price, second_currency)
-  first_price = convert_to_bgn(first_price, first_currency)
-  second_price = convert_to_bgn(second_price, second_currency)
-
-  first_price <=> second_price
+def compare_prices(price_a, currency_a, price_b, currency_b)
+  convert_to_bgn(price_a, currency_a) <=> convert_to_bgn(price_b, currency_b)
 end
